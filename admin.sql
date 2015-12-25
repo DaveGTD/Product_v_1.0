8cfe5425b943ef2aef7850b79f7f4e8475bbd689
Aditya Dave <dave.ceo.usa@gmail.com>
2015-12-25 21:39:04 +0000

    None

---
+++ admin.sql
@@ -0,0 +1,17 @@
+-- This SLQ file is for creating tables to manage user accounts 
+-- Database: product_v_1_0
+
+CREATE DATABASE product_v_1_0;
+
+USE product_v_1_0;
+
+CREATE TABLE accounts
+(
+	user_id INT NOT NULL AUTO_INCREMENT,
+	first_name varchar(255),
+	last_name varchar(255),
+	email varchar(255) NOT NULL,
+	-- TODO: Hash the passwords and change this to CHAR(FixedLength) 
+	password TEXT NOT NULL,
+	PRIMARY KEY (user_id) 
+);
---
+++ sample_data.sql
@@ -0,0 +1,8 @@
+-- This SQL file is for inserting test data into 'product_v_1_0' database 
+
+USE product_v_1_0;
+
+INSERT INTO accounts (email, password) VALUES ('dave@dave.com', 'dave');
+INSERT INTO accounts (email, password) VALUES ('anna@anna.com', 'anna');
+INSERT INTO accounts (email, password) VALUES ('rae@rae.com', 'rae');
+INSERT INTO accounts (email, password) VALUES ('someone@someone.com', 'someone');
