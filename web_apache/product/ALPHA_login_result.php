70c85d9317ae738f3e9f8a92da227d4ea6f26ac6
Aditya Dave <dave.ceo.usa@gmail.com>
2015-12-29 18:23:09 +0000

    None

--- web_apache/product/ALPHA_login_result.php
+++ web_apache/product/ALPHA_login_result.php
@@ -11,7 +11,7 @@ var_dump($_POST);
 	
 	if($answer)
 	{
-		header('Location: http://localhost:80/Product_v_1.0/web_apache/product/ALPHA_home.php');
+		header('Location: http://localhost:9901/Product_v_1.0/web_apache/product/ALPHA_home.php');
 	}
 	else
 	{
