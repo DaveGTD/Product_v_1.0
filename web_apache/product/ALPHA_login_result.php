33931e9dfd48eb03b927846731e083f70acaa81e
Aditya Dave <dave.ceo.usa@gmail.com>
2015-12-29 18:21:13 +0000

    none

--- web_apache/product/ALPHA_login_result.php
+++ web_apache/product/ALPHA_login_result.php
@@ -1,5 +1,22 @@
 <?php 
 
+include 'secret.inc';
 var_dump($_POST);	
+	
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$conn = getConnection();
+	
+	$answer = true;
+	
+	if($answer)
+	{
+		header('Location: http://localhost:80/Product_v_1.0/web_apache/product/ALPHA_home.php');
+	}
+	else
+	{
+		echo "Login Failed";
+	}
+	
 	
 ?>
