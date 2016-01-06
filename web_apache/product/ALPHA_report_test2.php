2f4d2ce1e1adaddda40293b56999e219d57a4064
Aditya Dave <dave.ceo.usa@gmail.com>
2016-01-06 18:56:06 +0000

    None

--- web_apache/product/ALPHA_report_test2.php
+++ web_apache/product/ALPHA_report_test2.php
@@ -26,49 +26,6 @@
   <div class="col-md-12 col-lg-10 col-lg-offset-1">
 
 
-
-    <div class="well well-sm">
-      <label>Theme:</label>
-      <div class="btn-group">
-        <button class="btn btn-primary btn-sm change-theme" data-qb="../dist/css/query-builder.default.min.css" data-bt="bower_components/bootstrap/dist/css/bootstrap.min.css">Default</button>
-        <button class="btn btn-primary btn-sm change-theme" data-qb="../dist/css/query-builder.dark.min.css" data-bt="bower_components/bootswatch-dist/css/bootstrap.min.css">Dark</button>
-      </div>
-      
-      <label>Language:</label>
-      <select name="language" class="selectpicker show-tick show-menu-arrow" data-width="auto">
-        <option value="sq" data-icon="flag flag-al">Albanian</option>
-        <option value="de" data-icon="flag flag-de">German</option>
-        <option value="da" data-icon="flag flag-dk">Danish</option>
-        <option value="en" data-icon="flag flag-gb" selected>English</option>
-        <option value="es" data-icon="flag flag-es">Spanish</option>
-        <option value="fr" data-icon="flag flag-fr">French</option>
-        <option value="it" data-icon="flag flag-it">Italian</option>
-        <option value="fa-IR" data-icon="flag flag-ir">Farsi</option>
-        <option value="nl" data-icon="flag flag-nl">Dutch</option>
-        <option value="no" data-icon="flag flag-no">Norwegian</option>
-        <option value="pl" data-icon="flag flag-pl">Polish</option>
-        <option value="pt-PT" data-icon="flag flag-pt-PT">Portuguese</option>
-        <option value="pt-BR" data-icon="flag flag-pt-BR">Brazilian Portuguese</option>
-        <option value="ro" data-icon="flag flag-ro">Romanian</option>
-        <option value="ru" data-icon="flag flag-ru">Russian</option>
-        <option value="zh-CN" data-icon="flag flag-zh-CN" >Simplified Chinese</option>
-      </select>
-    </div>
-
-    <div id="builder"></div>
-
-    <div class="btn-group">
-      <button class="btn btn-danger reset">Reset</button>
-      <button class="btn btn-warning set-filters" data-toggle="tooltip" title="Adds a filter 'New filter' and removes 'Coordinates', 'State', 'BSON'" data-container="body" data-placement="bottom">Set filters</button>
-    </div>
-
-    <div class="btn-group">
-      <button class="btn btn-default" disabled>Set:</button>
-      <button class="btn btn-success set">From JSON</button>
-      <button class="btn btn-success set-mongo">From MongoDB</button>
-      <button class="btn btn-success set-sql">From SQL</button>
-    </div>
-
     <div class="btn-group">
       <button class="btn btn-default" disabled>Get:</button>
       <button class="btn btn-primary parse-json">JSON</button>
@@ -175,116 +132,45 @@ var options = {
 	   default_value: 'Enter Date/Time/Date & Time',
 	   size: 100
 	   
-   }
-
-    
-    ]
-};
-
-// Fix for Selectize
-$('#builder-widgets').on('afterCreateRuleInput.queryBuilder', function(e, rule) {
-  if (rule.filter.plugin == 'selectize') {
-    rule.$el.find('.rule-value-container').css('min-width', '200px')
-      .find('.selectize-control').removeClass('form-control');
-  }
-});
-
-$('#builder-widgets').queryBuilder({
-  plugins: ['bt-tooltip-errors'],
-
-  filters: [{
-    id: 'date',
-    label: 'Datepicker',
-    type: 'date',
-    validation: {
-      format: 'YYYY/MM/DD'
-    },
-    plugin: 'datepicker',
-    plugin_config: {
-      format: 'yyyy/mm/dd',
-      todayBtn: 'linked',
-      todayHighlight: true,
-      autoclose: true
-    }
-  }, {
-    id: 'rate',
-    label: 'Slider',
-    type: 'integer',
-    validation: {
-      min: 0,
-      max: 100
-    },
-    plugin: 'slider',
-    plugin_config: {
-      min: 0,
-      max: 100,
-      value: 0
-    },
-    valueSetter: function(rule, value) {
-      rule.$el.find('.rule-value-container input').slider('setValue', value);
-    },
-    valueGetter: function(rule) {
-      return rule.$el.find('.rule-value-container input').slider('getValue');
-    }
-  }, {
-    id: 'category',
-    label: 'Selectize',
+   }, 
+   
+   {
+    id: 'State',
+    label: 'State',
     type: 'string',
+    input: 'select',
+    multiple: true,
     plugin: 'selectize',
     plugin_config: {
       valueField: 'id',
       labelField: 'name',
       searchField: 'name',
       sortField: 'name',
-      create: true,
-      maxItems: 1,
-      plugins: ['remove_button'],
-      onInitialize: function() {
-        var that = this;
-
-        if (localStorage.demoData === undefined) {
-          $.getJSON(baseurl + '/assets/demo-data.json', function(data) {
-            localStorage.demoData = JSON.stringify(data);
-            data.forEach(function(item) {
-              that.addOption(item);
-            });
-          });
-        }
-        else {
-          JSON.parse(localStorage.demoData).forEach(function(item) {
-            that.addOption(item);
-          });
-        }
-      }
+      options: [
+        { id: "AL", name: "Alabama" },
+        { id: "AK", name: "Alaska" },
+        { id: "AZ", name: "Arizona" },
+        { id: "AR", name: "Arkansas" },
+        { id: "CA", name: "California" },
+        { id: "CO", name: "Colorado" },
+        { id: "CT", name: "Connecticut" },
+        { id: "DE", name: "Delaware" },
+        { id: "DC", name: "District of Columbia" },
+        { id: "FL", name: "Florida" },
+        { id: "GA", name: "Georgia" },
+        { id: "HI", name: "Hawaii" },
+        { id: "ID", name: "Idaho" }
+      ]
     },
     valueSetter: function(rule, value) {
-      rule.$el.find('.rule-value-container input')[0].selectize.setValue(value);
+      rule.$el.find('.rule-value-container select')[0].selectize.setValue(value);
     }
-  },
-  
-     {
-	   id: 'Name',
-	   label: {en: 'Name'},
-	   type: 'string',
-	   // optgroup: 'Core',
-	   default_value: 'Enter Name',
-	   size: 100
-	   
-   },
-   
-   {
-	   id: 'Time',
-	   label: {en: 'Time'},
-	   type: 'datetime',
-	   // optgroup: 'Core',
-	   default_value: 'Enter Date/Time/Date & Time',
-	   size: 100
-	   
-   }
-  
-  ]
-  
-});
+
+    
+    ]
+};
+
+
 
 
 
@@ -298,7 +184,7 @@ $('#builder-widgets').queryBuilder({
 
 
 // init
-// $('#builder').queryBuilder(options);
+$('#builder').queryBuilder(options);
 
 $('#builder').on('afterCreateRuleInput.queryBuilder', function(e, rule) {
     if (rule.filter.plugin == 'selectize') {
@@ -307,33 +193,7 @@ $('#builder').on('afterCreateRuleInput.queryBuilder', function(e, rule) {
     }
 });
 
-// change language
-$('[name=language]').selectpicker().on('change', function() {
-  var lang = $(this).val();
-  
-  var done = function() {
-    var rules = $b.queryBuilder('getRules');
-    if (!$.isEmptyObject(rules)) {
-      options.rules = rules;
-    }
-    options.lang_code = lang;
-    $b.queryBuilder('destroy');
-    $('#builder').queryBuilder(options);
-  };
-  
-  if ($.fn.queryBuilder.regional[lang] === undefined) {
-    $.getScript('../dist/i18n/query-builder.' + lang + '.js', done);
-  }
-  else {
-    done();
-  }
-});
 
-// change theme
-$('.change-theme').on('click', function() {
-    $('#qb-theme').replaceWith('<link rel="stylesheet" href="' + $(this).data('qb') + '" id="qb-theme">');
-    $('#bt-theme').replaceWith('<link rel="stylesheet" href="' + $(this).data('bt') + '" id="bt-theme">');
-});
 
 // set rules
 $('.set').on('click', function() {
@@ -375,29 +235,7 @@ $('.set').on('click', function() {
   });
 });
 
-// set rules from MongoDB
-$('.set-mongo').on('click', function() {
-  $('#builder').queryBuilder('setRulesFromMongo', {
-    "$and": [{
-      "name": {
-        "$regex": "^(?!Mistic)"
-      }
-    }, {
-      "price": { "$gte": 0, "$lte": 100 }
-    }, {
-      "$or": [{
-        "category": 2
-      }, {
-        "category": { "$in": [4, 5] }
-      }]
-    }]
-  });
-});
 
-// set rules from SQL
-$('.set-sql').on('click', function() {
-  $('#builder').queryBuilder('setRulesFromSQL', 'name NOT LIKE "Mistic%" AND price BETWEEN 100 AND 200 AND (category IN(1, 2) OR rate <= 2)');
-});
 
 // reset builder
 $('.reset').on('click', function() {
@@ -430,28 +268,8 @@ $('.parse-mongo').on('click', function() {
     ));
 });
 
-// set filters
-$('.set-filters').on('click', function() {
-  $(this).prop('disabled', true);
-  $(this).tooltip('hide');
-  
-  // add a new filter after 'state'
-  $('#builder').queryBuilder('addFilter',
-    {
-      id: 'new_one',
-      label: 'New filter',
-      type: 'string'
-    },
-    'state'
-  );
-  
-  // remove filter 'coord'
-  $('#builder').queryBuilder('removeFilter',
-    ['coord', 'state', 'bson'],
-    true
-  );
-  
-  // also available : 'setFilters'
+
+
 });
 
 
