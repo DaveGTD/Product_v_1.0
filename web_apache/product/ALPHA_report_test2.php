64a5ab0be5d2b5ced89416339281d44022645d35
Aditya Dave <dave.ceo.usa@gmail.com>
2016-01-06 18:50:31 +0000

    None

--- web_apache/product/ALPHA_report_test2.php
+++ web_apache/product/ALPHA_report_test2.php
@@ -167,7 +167,6 @@ var options = {
 	   
    },
    
-/*
    {
 	   id: 'Time',
 	   label: {en: 'Time'},
@@ -176,30 +175,13 @@ var options = {
 	   default_value: 'Enter Date/Time/Date & Time',
 	   size: 100
 	   
-   },
-*/
+   }
 
-	{
-    id: 'date',
-    label: 'Datepicker',
-    type: 'date',
-    validation: {
-      format: 'YYYY/MM/DD'
-    },
-    plugin: 'datepicker',
-    plugin_config: {
-      format: 'yyyy/mm/dd',
-      todayBtn: 'linked',
-      todayHighlight: true,
-      autoclose: true
-    }
-    }
     
     ]
 };
 
 // Fix for Selectize
-/*
 $('#builder-widgets').on('afterCreateRuleInput.queryBuilder', function(e, rule) {
   if (rule.filter.plugin == 'selectize') {
     rule.$el.find('.rule-value-container').css('min-width', '200px')
@@ -278,63 +260,45 @@ $('#builder-widgets').queryBuilder({
     valueSetter: function(rule, value) {
       rule.$el.find('.rule-value-container input')[0].selectize.setValue(value);
     }
-  }, {
-    id: 'coord',
-    label: 'Coordinates',
-    type: 'string',
-    validation: {
-      format: /^[A-C]{1}.[1-6]{1}$/
-    },
-    input: function(rule, name) {
-      var $container = rule.$el.find('.rule-value-container');
-      
-      $container.on('change', '[name='+ name +'_1]', function(){
-        var h = '';
-        
-        switch ($(this).val()) {
-          case 'A':
-            h = '<option value="-1">-</option> <option value="1">1</option> <option value="2">2</option>';
-            break;
-          case 'B':
-            h = '<option value="-1">-</option> <option value="3">3</option> <option value="4">4</option>';
-            break;
-          case 'C':
-            h = '<option value="-1">-</option> <option value="5">5</option> <option value="6">6</option>';
-            break;
-        }
-        
-        $container.find('[name='+ name +'_2]').html(h).toggle(h!='');
-      });
-      
-      return '\
-      <select name="'+ name +'_1"> \
-        <option value="-1">-</option> \
-        <option value="A">A</option> \
-        <option value="B">B</option> \
-        <option value="C">C</option> \
-      </select> \
-      <select name="'+ name +'_2" style="display:none;"></select>';
-    },
-    valueGetter: function(rule) {
-      return rule.$el.find('.rule-value-container [name$=_1]').val()
-        +'.'+ rule.$el.find('.rule-value-container [name$=_2]').val();
-    },
-    valueSetter: function(rule, value) {
-      if (rule.operator.nb_inputs > 0) {
-        var val = value.split('.');
-        
-        rule.$el.find('.rule-value-container [name$=_1]').val(val[0]).trigger('change');
-        rule.$el.find('.rule-value-container [name$=_2]').val(val[1]).trigger('change');
-      }
-    }
-  }],
-
-  rules: rules_widgets
+  },
+  
+     {
+	   id: 'Name',
+	   label: {en: 'Name'},
+	   type: 'string',
+	   // optgroup: 'Core',
+	   default_value: 'Enter Name',
+	   size: 100
+	   
+   },
+   
+   {
+	   id: 'Time',
+	   label: {en: 'Time'},
+	   type: 'datetime',
+	   // optgroup: 'Core',
+	   default_value: 'Enter Date/Time/Date & Time',
+	   size: 100
+	   
+   }
+  
+  ]
+  
 });
-*/
+
+
+
+
+
+
+
+
+
+
+
 
 // init
-$('#builder').queryBuilder(options);
+// $('#builder').queryBuilder(options);
 
 $('#builder').on('afterCreateRuleInput.queryBuilder', function(e, rule) {
     if (rule.filter.plugin == 'selectize') {
