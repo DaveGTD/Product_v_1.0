e1091e71d72946c9a24b7fb4af85fdcf4641b1c5
DaveGTD <dave.ceo.usa@gmail.com>
2016-01-18 03:55:31 +0000

    none

--- web_apache/product/ALPHA_create_database2.php
+++ web_apache/product/ALPHA_create_database2.php
@@ -66,39 +66,39 @@
 		
 												 <script>
 										
-										function addRow(tableID) {
-											var table = document.getElementById(tableID);
-											var rowCount = table.rows.length;
-											if(rowCount < 5){							// limit the user from creating fields more than your limits
-												var row = table.insertRow(rowCount);
-												var colCount = table.rows[0].cells.length;
-												for(var i=0; i<colCount; i++) {
-													var newcell = row.insertCell(i);
-													newcell.innerHTML = table.rows[0].cells[i].innerHTML;
-												}
-											}else{
-												 alert("You need PRO to create more than 5 columns");
-													   
-											}
-										}
-
-										function deleteRow(tableID) {
-											var table = document.getElementById(tableID);
-											var rowCount = table.rows.length;
-											for(var i=0; i<rowCount; i++) {
-												var row = table.rows[i];
-												var chkbox = row.cells[0].childNodes[0];
-												if(null != chkbox && true == chkbox.checked) {
-													if(rowCount <= 1) { 						// limit the user from removing all the fields
-														alert("Must have atleast 1 column.");
-														break;
+												function addRow(tableID) {
+													var table = document.getElementById(tableID);
+													var rowCount = table.rows.length;
+													if(rowCount < 5){							// limit the user from creating fields more than your limits
+														var row = table.insertRow(rowCount);
+														var colCount = table.rows[0].cells.length;
+														for(var i=0; i<colCount; i++) {
+															var newcell = row.insertCell(i);
+															newcell.innerHTML = table.rows[0].cells[i].innerHTML;
+														}
+													}else{
+														 alert("Maximum Passenger per ticket is 5.");
+															   
 													}
-													table.deleteRow(i);
-													rowCount--;
-													i--;
 												}
-											}
-										}
+												
+												function deleteRow(tableID) {
+													var table = document.getElementById(tableID);
+													var rowCount = table.rows.length;
+													for(var i=0; i<rowCount; i++) {
+														var row = table.rows[i];
+														var chkbox = row.cells[0].childNodes[0];
+														if(null != chkbox && true == chkbox.checked) {
+															if(rowCount <= 1) { 						// limit the user from removing all the fields
+																alert("Cannot Remove all the Passenger.");
+																break;
+															}
+															table.deleteRow(i);
+															rowCount--;
+															i--;
+														}
+													}
+												}
 											
 											 
 										</script>
