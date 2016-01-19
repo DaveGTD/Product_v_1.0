8b757fd4109a46235d257b248ed869001aa3321e
DaveGTD <dave.ceo.usa@gmail.com>
2016-01-19 19:46:32 +0000

    none

---
+++ web_apache/product/ALPHA_report_test7.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Report Test</title>
+
+  <link rel="stylesheet" href="bower_components_upper/bootstrap/dist/css/bootstrap.min.css" id="bt-theme">
+  <link rel="stylesheet" href="bower_components_upper/bootstrap-select/dist/css/bootstrap-select.min.css">
+  <link rel="stylesheet" href="bower_components_upper/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css">
+  <link rel="stylesheet" href="bower_components/seiyria-bootstrap-slider/dist/css/bootstrap-slider.min.css">
+  <link rel="stylesheet" href="bower_components/selectize/dist/css/selectize.bootstrap3.css">
+
+  <link rel="stylesheet" href="query-builder.default.css" id="qb-theme">
+  
+  <link rel="stylesheet" href="http://mistic100.github.io/jQuery-QueryBuilder/assets/flags/flags.css">
+  <style>
+    .flag { display: inline-block; }
+  </style>
+</head>
+
+<body>
+
+<div class="container">
+  <div class="col-md-12 col-lg-10 col-lg-offset-1">
+
+  <div id="builder"></div>
+
+    <div class="btn-group">
+      <button class="btn btn-default" disabled>Get:</button>
+      <button class="btn btn-primary parse-json">JSON</button>
+      <button class="btn btn-primary parse-sql" data-stmt="false">SQL</button>
+      <button class="btn btn-primary parse-sql" data-stmt="question_mark">SQL statement</button>
+      <button class="btn btn-primary parse-mongo">MongoDB</button>
+    </div>
+
+    <div id="result" class="hide">
+      <h3>Output</h3>
+      <pre></pre>
+    </div>
+  </div>
+</div>
+
+<script src="bower_components_upper/jquery/dist/jquery.js"></script>
+<script src="bower_components_upper/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="bower_components_upper/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+<script src="bower_components_upper/bootbox/bootbox.js"></script>
+<script src="bower_components/seiyria-bootstrap-slider/dist/bootstrap-slider.min.js"></script>
+<script src="bower_components/selectize/dist/js/standalone/selectize.min.js"></script>
+<script src="bower_components_upper/jquery-extendext/jQuery.extendext.min.js"></script>
+<script src="bower_components_upper/sql-parser/browser/sql-parser.js"></script>
+<script src="bower_components_upper/doT/doT.js"></script>
+
+<script src="query-builder.js"></script>
+
+<script>
+	
+var rules_widgets = {
+  condition: 'OR',
+  rules: [{
+    id: 'date',
+    operator: 'equal',
+    value: '1991/11/17'
+  }, {
+    id: 'rate',
+    operator: 'equal',
+    value: 22
+  }, {
+    id: 'category',
+    operator: 'equal',
+    value: '38'
+  }, {
+    condition: 'AND',
+    rules: [{
+      id: 'coord',
+      operator: 'equal',
+      value: 'B.3'
+    }]
+  }]
+};
+
+// Fix for Selectize
+$('#builder-widgets').on('afterCreateRuleInput.queryBuilder', function(e, rule) {
+  if (rule.filter.plugin == 'selectize') {
+    rule.$el.find('.rule-value-container').css('min-width', '200px')
+      .find('.selectize-control').removeClass('form-control');
+  }
+});
+
+$('#builder-widgets').queryBuilder({
+  plugins: ['bt-tooltip-errors'],
+
+  filters: [{
+    id: 'date',
+    label: 'Date',
+    type: 'date',
+    validation: {
+      format: 'YYYY/MM/DD'
+    },
+    plugin: 'datepicker',
+    plugin_config: {
+      format: 'yyyy/mm/dd',
+      todayBtn: 'linked',
+      todayHighlight: true,
+      autoclose: true
+    }
+  }, {
+    id: 'rate',
+    label: 'Size',
+    type: 'integer',
+    validation: {
+      min: 0,
+      max: 100
+    },
+    plugin: 'slider',
+    plugin_config: {
+      min: 0,
+      max: 100,
+      value: 0
+    },
+    valueSetter: function(rule, value) {
+      if (rule.operator.nb_inputs == 1) value = [value];
+      rule.$el.find('.rule-value-container input').each(function(i) {
+        $(this).slider('setValue', value[i] || 0);
+      });
+    },
+    valueGetter: function(rule) {
+      var value = [];
+      rule.$el.find('.rule-value-container input').each(function() {
+        value.push($(this).slider('getValue'));
+      });
+      return rule.operator.nb_inputs == 1 ? value[0] : value;
+    }
+  }, {
+    id: 'category',
+    label: 'Selectize',
+    type: 'string',
+    plugin: 'selectize',
+    plugin_config: {
+      valueField: 'id',
+      labelField: 'name',
+      searchField: 'name',
+      sortField: 'name',
+      create: true,
+      maxItems: 1,
+      plugins: ['remove_button'],
+      onInitialize: function() {
+        var that = this;
+
+        if (localStorage.demoData === undefined) {
+          $.getJSON(baseurl + '/assets/demo-data.json', function(data) {
+            localStorage.demoData = JSON.stringify(data);
+            data.forEach(function(item) {
+              that.addOption(item);
+            });
+          });
+        }
+        else {
+          JSON.parse(localStorage.demoData).forEach(function(item) {
+            that.addOption(item);
+          });
+        }
+      }
+    },
+    valueSetter: function(rule, value) {
+      rule.$el.find('.rule-value-container input')[0].selectize.setValue(value);
+    }
+  }, {
+    id: 'coord',
+    label: 'Coordinates',
+    type: 'string',
+    validation: {
+      format: /^[A-C]{1}.[1-6]{1}$/
+    },
+    input: function(rule, name) {
+      var $container = rule.$el.find('.rule-value-container');
+      
+      $container.on('change', '[name='+ name +'_1]', function(){
+        var h = '';
+        
+        switch ($(this).val()) {
+          case 'A':
+            h = '<option value="-1">-</option> <option value="1">1</option> <option value="2">2</option>';
+            break;
+          case 'B':
+            h = '<option value="-1">-</option> <option value="3">3</option> <option value="4">4</option>';
+            break;
+          case 'C':
+            h = '<option value="-1">-</option> <option value="5">5</option> <option value="6">6</option>';
+            break;
+        }
+        
+        $container.find('[name='+ name +'_2]').html(h).toggle(h!='');
+      });
+      
+      return '\
+      <select name="'+ name +'_1"> \
+        <option value="-1">-</option> \
+        <option value="A">A</option> \
+        <option value="B">B</option> \
+        <option value="C">C</option> \
+      </select> \
+      <select name="'+ name +'_2" style="display:none;"></select>';
+    },
+    valueGetter: function(rule) {
+      return rule.$el.find('.rule-value-container [name$=_1]').val()
+        +'.'+ rule.$el.find('.rule-value-container [name$=_2]').val();
+    },
+    valueSetter: function(rule, value) {
+      if (rule.operator.nb_inputs > 0) {
+        var val = value.split('.');
+        
+        rule.$el.find('.rule-value-container [name$=_1]').val(val[0]).trigger('change');
+        rule.$el.find('.rule-value-container [name$=_2]').val(val[1]).trigger('change');
+      }
+    }
+  }],
+
+  rules: rules_widgets
+});
+
+$('#btn-reset').on('click', function() {
+  $('#builder-widgets').queryBuilder('reset');
+});
+
+$('#btn-set').on('click', function() {
+  $('#builder-widgets').queryBuilder('setRules', rules_widgets);
+});
+
+$('#btn-get').on('click', function() {
+  var result = $('#builder-widgets').queryBuilder('getRules');
+  
+  if (!$.isEmptyObject(result)) {
+    alert(JSON.stringify(result, null, 2));
+  }
+});
+
+</script>
+
+</body>
+</html>
